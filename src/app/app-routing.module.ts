4b9060c6b3953dd5487eb240e8288d723d36672e
Arpad <solar@hu.inter.net>
2021-03-26 17:24:13 +0100

    table thead

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './page/user-list/user-list.component';
 
 const routes: Routes = [
+  /*table*/
   {
     path: '',
     component: UserListComponent,
   },
+  /*table*/
   {
     path: '**',
     component: UserListComponent,
